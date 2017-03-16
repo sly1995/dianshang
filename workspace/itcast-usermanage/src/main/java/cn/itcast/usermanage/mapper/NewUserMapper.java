0f293ccbbb11e8b5bf39855bfc63785d79fb3f84
sly1995 <1562110092@qq.com>
2017-03-16 15:19:42 +0800

    delete

--- workspace/itcast-usermanage/src/main/java/cn/itcast/usermanage/mapper/NewUserMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.usermanage.mapper;
-
-import cn.itcast.usermanage.pojo.User;
-
-import com.github.abel533.mapper.Mapper;
-
-public interface NewUserMapper extends Mapper<User> {
-
-}
